0ee55d03a929dd69a55d5d137f409f5669c03adc
Bijay Khadka <bk006822@gmail.com>
2023-08-23 22:45:20 +0545

    added: custom user and user profile model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    
+    # Adding related_name for groups and user_permissions fields
+    groups = models.ManyToManyField(Group, related_name='custom_users', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_users', blank=True)
+    
+    def __str__(self):
+        return self.username
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=1000, null=True, blank=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True)
+    
+    def __str__(self):
+        return self.user.username
+    
