a2d4f60de44b3c651cad39d1a03805c9a38d9ef3
Bijay Khadka <bk006822@gmail.com>
2023-08-24 21:40:08 +0545

    Models: Posts, Like, Comment

--- authentication/models.py
+++ authentication/models.py
@@ -16,6 +16,12 @@ class UserProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     bio = models.TextField(max_length=1000, null=True, blank=True)
     profile_image = models.ImageField(upload_to='profile_images/', blank=True)
+    follows = models.ManyToManyField(
+        "self",
+        related_name="followed_by",
+        symmetrical=False,
+        blank=True
+    )
     
     def __str__(self):
         return self.user.username
--- socialapp/models.py
+++ socialapp/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
+from authentication.models import CustomUser
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts', validators=[FileExtensionValidator(['png', 'jpg', 'jpeg'])], blank=True) 
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.content[:20])
+
+    def num_likes(self):
+        return self.like_set.count()
+
+    def num_comments(self):
+        return self.comment_set.count()
+
+    class Meta:
+        ordering = ('-created',)
+        
+class Comment(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField(max_length=500)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.pk)
+
+class Like(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    is_like = models.BooleanField(default=True) 
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user}-{self.post}-{self.is_like}"
