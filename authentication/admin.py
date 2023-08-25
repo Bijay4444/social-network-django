d006790763a874ad400c80fb0692d6ea46cff054
Bijay Khadka <bk006822@gmail.com>
2023-08-25 22:28:07 +0545

    added: templates

--- authentication/admin.py
+++ authentication/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import CustomUser, UserProfile
 
 # Register your models here.
-admin.site.register(CustomUser)
-admin.site.register(UserProfile)
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    inlines = (UserProfileInline,)
+
