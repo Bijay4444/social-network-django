c02ccaff9a18bdf92b782f1c43e2fe335f1abebd
Bijay Khadka <bk006822@gmail.com>
2023-08-27 21:28:55 +0545

    user-profile consistency with signal

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -11,6 +13,15 @@ class CustomUser(AbstractUser):
     
     def __str__(self):
         return self.username
+    
+#Defining a signal receiver function to create a user profile whenever a new CustomUser is created
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+#This ensures that the user-profile relationship is maintained consistently
+post_save.connect(create_user_profile, sender=CustomUser)
     
 class UserProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
