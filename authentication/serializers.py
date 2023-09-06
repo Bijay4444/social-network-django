d3cf3b6f5ccba5fba3c720c3f761cf3b75b5a7d0
Bijay Khadka <bk006822@gmail.com>
2023-09-06 19:47:19 +0545

    Updated Authentication

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -2,23 +2,28 @@ from rest_framework import serializers
 from . import models
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+    id = serializers.IntegerField(read_only=True)
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
     class Meta:
-        model = models.CustomUser
-        fields = ('id', 'email', 'username', 'first_name', 'last_name', 'password', 'date_of_birth')
-        extra_kwargs = {'password': {'write_only': True}}
+            model = models.CustomUser
+            fields = ['id', 'email','username', 'password', 'confirm_password']
         
+    def validate(self, attrs):
+            if attrs.get('password')!=attrs.get('confirm_password'):
+                raise serializers.ValidationError("Password and Confirm Password does not match")
+        
+            return attrs
+    
     def create(self, validated_data):
-        user = models.CustomUser(
-            email=validated_data['email'],
-            username=validated_data['username'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            date_of_birth=validated_data['date_of_birth'],
-        )
-        user.set_password(validated_data['password'])
+        password = validated_data.pop('password')
+        validated_data.pop('confirm_password')  # Remove confirm_password from validated_data
+
+        user = models.CustomUser(**validated_data)
+        user.set_password(password)
         user.save()
         return user
-
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -27,12 +32,10 @@ class UserProfileSerializer(serializers.ModelSerializer):
         
 
 class UserLoginSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    username = serializers.CharField(max_length=255)
+    id= serializers.IntegerField(read_only=True)
+    username=serializers.CharField(max_length=255)
     password=serializers.CharField(write_only=True)
     token=serializers.CharField(read_only=True)
-    
     class Meta:
-        model = models.CustomUser
-        fields = ('id', 'username', 'password' , 'token')
-        extra_kwargs = {'password': {'write_only': True}}
+        model=models.CustomUser
+        fields=['id','username','password','token']
